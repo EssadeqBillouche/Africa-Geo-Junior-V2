40efb5c52565f1722622dd4c611292f7531e3023
Radioserk <boujahdachraf@gmail.com>
2024-12-26 15:34:53 +0100

    updating the credit

--- index.php
+++ index.php
@@ -244,11 +244,7 @@
                 <a href=""><i class="bi bi-linkedin"></i></a>
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you've purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-                Designed by <a href="https://bootstrapmade.com/">CodeShogne</a> Distributed by <a href="https://themewagon.com">Africa Explorer</a>
+                Designed by <a>CodeShogne</a>
             </div>
         </div>
 
